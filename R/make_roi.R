114803667a6e829e852c904ecc88d786d20079bd
Eric Scott <ericrscott@arizona.edu>
2024-11-18 11:11:38 -0700

    revert ROI to CASC NE states

--- R/make_roi.R
+++ R/make_roi.R
@@ -1,28 +1,26 @@
+#States in NE CASC: https://www.usgs.gov/programs/climate-adaptation-science-centers/northeast-casc
 make_roi <- function() {
-  maps::map(database = "state", regions = c(
-    "Connecticut",
-    "Delaware",
-    "Illinois",
-    "Indiana",
-    "Iowa",
-    "Maine",
-    "Maryland",
-    "Massachusetts",
-    "Michigan",
-    "Minnesota",
-    "Missouri",
-    "New Hampshire",
-    "New Jersey",
-    "New York",
-    "Ohio",
-    "Pennsylvania",
-    "Rhode Island",
-    "Vermont",
-    "West Virginia",
-    "Wisconsin",
-    "Kentucky",
-    "Virginia"
-  ), plot = FALSE, fill = TRUE) |> 
+  maps::map(
+    database = "state",
+    regions = c(
+      "Connecticut",
+      "Delaware",
+      "Kentucky",
+      "Maine",
+      "Maryland",
+      "Massachusetts",
+      "New Hampshire",
+      "New Jersey",
+      "New York",
+      "Pennsylvania",
+      "Rhode Island",
+      "Virginia",
+      "Vermont",
+      "West Virginia"
+    ),
+    plot = FALSE,
+    fill = TRUE
+  ) |> 
     sf::st_as_sf() |> 
     sf::st_combine() |> 
     terra::vect()
